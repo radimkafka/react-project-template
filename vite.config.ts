fd9bc041daf5d9888fdd7c395c8268ce254bca43
Radim Kafka <kafkaradim@gmail.com>
2025-02-01 11:26:58 +0100

    checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: { tsconfigPath: "./tsconfig.app.json" },
+      biome: {
+        command: "check",
+      },
+    }),
+  ],
 });
