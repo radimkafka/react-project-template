3e589d080e31380c2789cc3aa8b89e90f292e1aa
Radim Kafka <kafkaradim@gmail.com>
2025-02-01 11:50:34 +0100

    tanstack router init

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
@@ -5,6 +6,7 @@ import checker from "vite-plugin-checker";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
     checker({
       typescript: { tsconfigPath: "./tsconfig.app.json" },
