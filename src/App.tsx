f730f0c9ec73157b599a678d16fc479d0fc8d6e7
Radim Kafka <kafkaradim@gmail.com>
2025-02-05 22:44:34 +0100

    api

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { ThemeProvider } from "./components/theme-provider";
 import { routeTree } from "./routeTree.gen";
 import "./translations/i18n";
+import { useEffect } from "react";
+import { getS, getT, setupClient } from "./api/Api";
 
 const router = createRouter({ routeTree });
 
@@ -12,6 +14,24 @@ declare module "@tanstack/react-router" {
 }
 
 function App() {
+  setupClient({
+    apiUrl: `${import.meta.env.VITE_API_URL}`,
+    jwtKey: undefined,
+    requestMiddlewares: [
+      {
+        name: "apikey",
+        fn: (req) => {
+          req.url = req.url.replace("s?", `apikey=${import.meta.env.VITE_API_KEY}&`);
+          req.options.headers = {};
+          return req;
+        },
+      },
+    ],
+  });
+  useEffect(() => {
+    getS("batman").then(console.log);
+  }, []);
+
   return (
     <ThemeProvider>
       <RouterProvider router={router} />
